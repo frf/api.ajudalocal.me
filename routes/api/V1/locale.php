2cf735d918f7bbd407684a6ac9122bd734f4483e
Fabio Rocha <fabio@fabiofarias.com.br>
2021-02-21 16:05:10 -0300

    Locale

--- routes/api/V1/locale.php
+++ routes/api/V1/locale.php
@@ -4,14 +4,17 @@ use App\Api\Controllers\FileController;
 use App\Api\Controllers\LocaleController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('locales')->group(function () {
+    Route::get('/', [LocaleController::class, 'index'])
+        ->name('locale.index');
+});
+
 Route::prefix('locales')->middleware(['auth:api','user_context'])
     ->group(function () {
-        Route::get('/', [LocaleController::class, 'index'])
-            ->name('locale.index');
-
         Route::post('/', [LocaleController::class, 'create'])
             ->name('locale.create');
 
         Route::patch('/', [LocaleController::class, 'update'])
             ->name('locale.update');
-    });
+});
+
